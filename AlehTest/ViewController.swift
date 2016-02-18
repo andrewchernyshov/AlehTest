a0605aad6fff852a5bb9596195c774064ee66c72
Andrei Chernushou <chernyshev.andy@gmail.com>
2016-02-18 23:22:42 +0300

    step 1

--- AlehTest/ViewController.swift
+++ AlehTest/ViewController.swift
@@ -8,82 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
-    var imageView = UIImageView()
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let tableView = UITableView()
+    let tableviewDataStack = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        self.view.addSubview(imageView)
-        imageView.backgroundColor = UIColor.brownColor()
-        
-//        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-[imageView]-bottomPadding-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: ["bottomPadding|950": self.view.frame.size.height/2], views: ["imageView":imageView])
-        
-        self.navigationItem.rightBarButtonItems = [UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target:self, action: "addButtonPressed:")]
         
     }
-    
+
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         
-        let imageViewHeight = self.view.frame.size.height/2
-        
-        let heightConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Height, relatedBy: NSLayoutRelation.Equal, toItem: nil, attribute: NSLayoutAttribute.NotAnAttribute, multiplier: 1.0, constant: imageViewHeight)
-        heightConstraint.priority = 950
-        
-        let topConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Top, multiplier: 1.0, constant: 0.0)
-        
-        let bottomConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: self.view, attribute: NSLayoutAttribute.Bottom, multiplier: 0.5, constant: 0.0)
-        
-        let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("H:|[imageView]|", options: NSLayoutFormatOptions(rawValue: 0), metrics: [:], views: ["imageView":imageView])
-        imageView.setContentCompressionResistancePriority(UILayoutPriorityRequired, forAxis: UILayoutConstraintAxis.Vertical)
-        
-        NSLayoutConstraint.activateConstraints(horizontalConstraints+[bottomConstraint, topConstraint, heightConstraint])
-        
+        let verticalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("V:|-topGuideLine-[tableview]-|", options: [], metrics: ["topGuideLine":self.topLayoutGuide], views: ["tableview": self.tableView])
+        let horizontalConstraints = NSLayoutConstraint.constraintsWithVisualFormat("|[tableview]|", options: [], metrics: [:], views: ["tableview":self.tableView])
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activateConstraints(verticalConstraints + horizontalConstraints)
     }
     
-    func addButtonPressed (sender:UIBarButtonItem){
-        
-        print("addButtonPressed")
-        
-        let imagePicker = UIImagePickerController()
-        imagePicker.delegate = self
-        
-        let actionSheet = UIAlertController.init(title: "Options", message: "Make your choise", preferredStyle: UIAlertControllerStyle.ActionSheet)
-        
-        let cancelAction = UIAlertAction.init(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
-        
-        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
-            
-            let takePhotoAction = UIAlertAction.init(title: "Take a photo", style: UIAlertActionStyle.Default) { (takePhotoAction) -> Void in
-                
-                imagePicker.sourceType = UIImagePickerControllerSourceType.Camera
-                self.presentViewController(imagePicker, animated: true, completion: nil)
-            }
-            actionSheet.addAction(takePhotoAction)
-        }
-        
-        let choosePhoto = UIAlertAction.init(title: "Choose from photo library", style: UIAlertActionStyle.Default) { (choosePhoto) -> Void in
-            imagePicker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
-            self.presentViewController(imagePicker, animated: true, completion: nil)
-            
-        }
-    
-        actionSheet.addAction(cancelAction)
-        actionSheet.addAction(choosePhoto)
-        self.presentViewController(actionSheet, animated: true, completion: nil)
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
     
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tableviewDataStack.count
+    }
     
-    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-        let chosenPhoto = info as Dictionary!
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
-        let image = chosenPhoto [UIImagePickerControllerOriginalImage] as! UIImage
+        let cell = UITableViewCell()
         
-        self.imageView.image = image
-        self.dismissViewControllerAnimated(true, completion: nil)
+        return cell
     }
     
     override func didReceiveMemoryWarning() {
